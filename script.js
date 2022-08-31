d0b6e5119c40a42502a3d3effc5f0d0b4bbb6174
juleberry <105573843+juleberry@users.noreply.github.com>
2022-08-31 12:31:18 -0400

    fix revealTreasures function

--- script.js
+++ script.js
@@ -2,76 +2,78 @@
 // **** TREASURE HUNT ****
 // *-*-*-*-*-*-*-*-*-*-*-*
 
-// -- create sorted treasure map --
+// store values for the Rounds, treasuresFound and treasureMap indices
+let round = 0;
+// let search = treasureMap[0]
+let currentRound = round + 1
+let num = 0
+let token = 0
+
+// use below for testing
+// let treasureMap = [
+//   1,
+//   "Treasure 6",
+//   7,
+//   6,
+//   "Treasure 5",
+//   9,
+//   10,
+//   "Treasure 3",
+//   12,
+//   11,
+//   3,
+//   "Treasure 1",
+//   13,
+//   "Treasure 4",
+//   8,
+//   2,
+//   "Treasure 7",
+//   4,
+//   5,
+//   "Treasure 2",
+// ];
+
+// -- begin creation of sorted treasure map --
+let treasureMap = []
+
 const loadMap = () => {
   // create array to hold numbers from 1-20 and the 7 treasure locations will replace 7 of the numbers
   // create array of treasures 'T'; default is 7 (can be expanded later)
   // add number to each "T" to test indexOf for testing
   // -----
-  // const treasures = []
-  // for (i = 1; i <= 7; i++) {
-  //   treasures.push('Treasure ' + i);
-  // }
+  const treasures = []
+  for (i = 1; i <= 7; i++) {
+    treasures.push('Treasure ' + i);
+  }
   // -----
 
   // array of numbers 1 through 20
   // treasure locations will need to subtract from the amount of treasures "hidden" within the game
-  // use code below for gameplay
   // -----
-  // const treasureLocations = []
-  // for (i = 1; i <= 20 - treasures.length; i++) {
-  //   treasureLocations.push(i)
-  // }
+  const treasureLocations = []
+  for (i = 1; i <= 20 - treasures.length; i++) {
+    treasureLocations.push(i)
+  }
   // -----
 
   // join arrays treasureLocations and treasures into new array, treasure map with concat method
-  // use code below for gameplay
   // -----
-  // const treasureMap = treasureLocations.concat(treasures)
+  treasureMap = treasureLocations.concat(treasures)
   // -----
-  // use below for testing
-  let treasureMap = [
-    1,
-    "Treasure 6",
-    7,
-    6,
-    "Treasure 5",
-    9,
-    10,
-    "Treasure 3",
-    12,
-    11,
-    3,
-    "Treasure 1",
-    13,
-    "Treasure 4",
-    8,
-    2,
-    "Treasure 7",
-    4,
-    5,
-    "Treasure 2",
-  ];
-  // tested location of treasure 5
-  // console.log(treasureMap.indexOf("T5"))
-  console.log(treasureMap);
 
   // --- shuffle treasures within treasureMap
-  // const shuffle = (totalTreasures) => {
+  const shuffle = (totalTreasures) => {
   // randomly sort the treasures within the array
-  //   totalTreasures.sort(() => Math.random() - 0.5)
-  //   return;
-  // }
+    totalTreasures.sort(() => Math.random() - 0.5)
+    return;
+  }
   // shuffles treasureMap
   // -----
-  // shuffle(treasureMap)
-  // console.log(treasureMap)
+  shuffle(treasureMap)
+  console.log(treasureMap)
   // ----
-  // store values for the Rounds and treasureMap indices
-  let round = 0;
-  // let search = treasureMap[0]
-  let currentRound = round + 1
-};
+  };
+  // --- end of sorted treasure map section ---
 
 //  -- set up players - class --
 // window prompt to enter name and choices for later
@@ -82,68 +84,94 @@ let humanName = "Player 1";
 // create 5 buttons and click will be user input for variable humanChoice
 let humanChoice = Math.floor(Math.random() * 5) + 1;
 let cpuChoice = Math.floor(Math.random() * 5) + 1;
-// console.log(humanChoice)
 
 class Player {
   constructor(name, choice, treasuresFound) {
     (this.name = name), // activate window prompt later
-      (this.choice = choice), //
-      (this.treasuresFound = treasuresFound),
-      (this.revealTreasure = this.revealTreasure.bind(this));
+    (this.choice = choice), // connect choice buttons to images later
+    (this.treasuresFound = token),
+    (this.revealTreasure = this.revealTreasure.bind(this));
   }
   // determine if treasure was found by testing for a string
-  revealTreasure(search) {
-    let i = 0
-    let search = treasureMap[i]
-    if (typeof search === "string" && isWinner === true) {
+  revealTreasure(index) {
+    let num = index
+    let search = treasureMap[index]
+    if (currentRound <= treasureMap.length) {
+    if (typeof search === "string") {
+      if (isWinner) {
       console.log(`X marks the spot! ${this.name} found a treasure!`);
-      // then, increase Round by 1, currentRound by 1, and treasureMap[i] by 1
-    } else if (typeof search === "string" && isWinner === false) {
+      // increase winner's found treasures by 1
+      this.treasuresFound = token++
+      // then, increase Round by 1, currentRound by 1, and treasureMap[i] by 1 and return to showChoices()
+      num++
+      nextRound()
+      }
+    } else if (typeof search === "string") {
+      if (!isWinner) {
       console.log('A treasure was left behind!')
-      // then, increase Round by 1, currentRound by 1, and treasureMap[i] by 1
+      // then, increase Round by 1, currentRound by 1, and treasureMap[i] by 1 and return to showChoices()
+      num++
+      nextRound()
+      }
     } else {
       console.log(`Oh no! ${this.name} found a rock!`);
-      // then, increase Round by 1, currentRound by 1, and treasureMap[i] by 1
+      // then, increase Round by 1, currentRound by 1, and treasureMap[i] by 1 and return to showChoices()
+      num++
+      nextRound()
     }
   }
 }
+}
 
-const humanPlayer = new Player(humanName, humanChoice);
-const cpuPlayer = new Player("CPU", cpuChoice);
+const humanPlayer = new Player(humanName, humanChoice, token);
+const cpuPlayer = new Player("CPU", cpuChoice, token);
 
 // randomly determine which player chooses first
 const firstChoice = () => {
+  console.log("Let's see who chooses first...")
   let human = Math.floor(Math.random() * 2) + 1;
   let cpu = Math.floor(Math.random() * 2) + 1;
   if (human === cpu) {
     console.log(`${humanPlayer.name} chooses first!`);
-    console.log(`${humanPlayer.name} chooses ${humanPlayer.choice}. `);
-    console.log(`${cpuPlayer.name} chose ${cpuPlayer.choice}`);
-
-    pickSpot()
+    showChoices()
   } else {
     console.log(`The computer chooses first!`);
-    console.log(`${cpuPlayer.name} chose ${cpuPlayer.choice}`);
-    console.log(`${humanPlayer.name} chooses ${humanPlayer.choice}. `);
-    pickSpot()
+    showChoices()
   }
 };
 
 // to win space, code will check if contents of the array index is a string (true or false) and will output the winner based on comparison of user's chosen number vs cpu's randomized number between 1 and maybe 6
+const showChoices = () => {
+if (humanChoice > cpuChoice) {
+  console.log(`${humanPlayer.name} chooses ${humanPlayer.choice}.`);
+  console.log(`${cpuPlayer.name} chose ${cpuPlayer.choice}.`);
+  pickSpot()
+  } else {
+  console.log(`${humanPlayer.name} chooses ${humanPlayer.choice}.`);
+  console.log(`${cpuPlayer.name} chose ${cpuPlayer.choice}.`);
+  pickSpot()
+  }
+}
 const pickSpot = () => {
+  do {
   if (isTie() === true) {
     console.log(`It's a tie! Let's see if there was a treasure...`);
-    cpuPlayer.revealTreasure(search)
+    cpuPlayer.revealTreasure(round)
   } else if (isWinner() === true) {
-    console.log(`${humanPlayer.name} wins this space! Let's see if there was a treasure...`);
+    console.log(`${humanPlayer.name} digs for a treasure!\nLet's see what you found...`);
     // then reveal if space is treasure and log to winner's score
-    humanPlayer.revealTreasure(search)
+    humanPlayer.revealTreasure(round)
   } else {
-    console.log(`${cpuPlayer.name} wins this space! Let's see if there was a treasure...`);
-    // then reveal if space is treasure and do not log to any player's score
-    cpuPlayer.revealTreasure(search)
+    console.log(`${cpuPlayer.name} digs for a treasure!\nLet's see if there was a treasure...`);
+    // then reveal if space is treasure and do not log to any player's 
+    cpuPlayer.revealTreasure(round)
   }
-};
+} while (currentRound <= 20)
+if (currentRound > 20) {
+  endGame()
+}
+}
+
 const isTie = () => {
   if (humanChoice === cpuChoice) {
     return true;
@@ -159,26 +187,45 @@ const isWinner = () => {
   }
 };
 
-// testing function -- remove later
-// console.log('should be zero =>' + round)
-// console.log('The current round should be one =>' + currentRound)
-// console.log('the space is currently => ' + search)
-// console.log(isString(search))
-// round++
-// currentRound++
-// search = treasureMap[1]
-// console.log('should be 1 =>' + round)
-// console.log('The current round should be two =>' + currentRound)
-// console.log('the space is currently => ' + search)
-// console.log(isString(search))
-// -----
+const nextRound = () => {
+  round++
+  currentRound++
+  console.log(`Round ${currentRound}.\nGet ready to choose a space...`)
+  showChoices()
+}
 
 // ------------------------
 //   **** START GAME ****
 // ------------------------
-// const startGame = () => {
-//   loadMap()
-//   firstChoice()
-// }
+const startGame = () => {
+  console.log(`Round ${currentRound}`)
+  loadMap()
+  firstChoice()
+}
+
+// =================
+// *** END GAME ***
+// =================
+// Game will end and reveal the total of how many treasures found and determine winner of entire game
+const endGame = () => {
+  console.log(`It's been a great treasure hunt!\nLet's see who won...`)
+  if (humanPlayer.treasuresFound > cpuPlayer.treasuresFound) {
+    console.log(`${humanPlayer.name} collected ${humanPlayer.treasuresFound} treasures.`)
+    console.log(`${cpuPlayer.name} collected ${cpuPlayer.treasuresFound} treasures.`)
+    console.log(`${humanPlayer.name} wins!`)
+    return
+  } else if (humanPlayer.treasuresFound === cpuPlayer.treasuresFound) {
+    console.log(`${cpuPlayer.name} collected ${cpuPlayer.treasuresFound} treasures.`)
+    console.log(`${humanPlayer.name} collected ${humanPlayer.treasuresFound} treasures.`)
+    console.log(`It's a tie! Please play again.`)
+    return
+  } else {
+    console.log(`${humanPlayer.name} collected ${humanPlayer.treasuresFound} treasures.`)
+    console.log(`${cpuPlayer.name} collected ${cpuPlayer.treasuresFound} treasures.`)
+    console.log(`${cpuPlayer.name} wins! Better luck next time.`)
+  }
+}
 
 // startGame()
+
+// fix infinite loop at end of game
